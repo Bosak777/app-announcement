31b10c0447ed68038035229e9e3b6d655800848c
Koo-raven-ks <raven_ks@icloud.com>
2026-01-27 19:46:56 +0900

    urlsyonmoku

--- python_apps_django/urls.py
+++ python_apps_django/urls.py
@@ -19,9 +19,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('word_wolf/', include('word_wolf.urls')),
-    path('card_game/', include('card_game.urls')),
-    path('baseball/', include('baseball.urls')),
-    path('yonmoku_narabe/', include('yonmoku_narabe.urls')),
+    path("admin/", admin.site.urls),
+    path("word_wolf/", include("word_wolf.urls")),
+    path("card_game/", include("card_game.urls")),
+    path("baseball/", include("baseball.urls")),
+    path("yonmoku_narabe/", include("yonmoku_narabe.urls")),
 ]
