69026faef53bc7a86de466eb63ab5e7161dd37e6
Bosak777 <masafujurasaki1126@gmail.com>
2026-01-28 10:02:11 +0900

    ローカル

--- python_apps_django/settings.py
+++ python_apps_django/settings.py
@@ -171,19 +171,3 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-##ここから下を追加##
-CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://127.0.0.1",
-]
-ALLOWED_HOSTS = [
-    "localhost",
-    "127.0.0.1",
-]
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
